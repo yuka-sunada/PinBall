d678ec42cecfc135fd70599e52990696a45edd1c
砂田祐佳 <yuka-sunada@united.jp>
2017-01-29 18:14:50 +0900

    Lesson5　スコア表示課題
    ボールがターゲットに表示された時に得点を表示させました

---
+++ Assets/BallController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	//ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+
+	//得点を表示させるテキスト
+	private GameObject gameScoreText;
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+
+		//シーン中のgameScoreTextオブジェクトを取得
+		this.gameScoreText = GameObject.Find("GameScoreText");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+			//GameoverTextにゲームオーバを表示
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+		}
+	}
+
+	//衝突時に呼ばれる関数
+	void OnCollisionEnter(Collision other) {
+		if (tag == "SmallStarTag") {
+			this.gameScoreText.GetComponent<Text> ().text = "10";
+		} else if (tag == "LargeStarTag") {
+			this.gameScoreText.GetComponent<Text> ().text = "20";
+		}else if(tag == "SmallCloudTag" || tag == "LargeCloudTag") {
+			this.gameScoreText.GetComponent<Text> ().text = "30";
+		}
+	}
+
+}
